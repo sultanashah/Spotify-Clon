b9cc8c265ceff35b60b9faf689748ddd7ec42a92
shah <shaistasultana.in@gmail.com>
2023-12-01 00:05:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("welcome to spotify")
+let songs =[
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+    {songsName:"salam-e-ishq", filePath:"covers/1.jpg"},
+
+]
+let audioelement =new Audio('1.mp3');
+// audioelement.play();
